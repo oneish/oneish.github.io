1c72cc871412103fb2d8e34db0edbbafc6f10530
oneish <stuartdavis@oneish.com>
2025-08-05 09:31:42 +0100

    data links

--- jukebox-app/scripts/data.js
+++ jukebox-app/scripts/data.js
@@ -1,14 +1,14 @@
 // Data module for songs
 export const songs = [
-  { title: '01 i am a strange loop', artist: 'oneish', file: 'https://drive.google.com/file/d/1UCjK8QZEkqnhqPbppyxmCKYE9Xgnnhqh' },
-  { title: '02 incognito', artist: 'oneish', file: 'assets/audio/02.mp3' },
-  { title: '03 lsd and the mind of the universe', artist: 'oneish', file: 'assets/audio/03.mp3' },
-  { title: '04 storming heaven', artist: 'oneish', file: 'assets/audio/04.mp3' },
-  { title: '05 the cosmic serpent', artist: 'oneish', file: 'assets/audio/05.mp3' },
-  { title: '06 real magic', artist: 'oneish', file: 'assets/audio/06.mp3' },
-  { title: '07 prometheus rising', artist: 'oneish', file: 'assets/audio/07.mp3' },
-  { title: '08 monsters and magical sticks', artist: 'oneish', file: '08.mp3' },
-  { title: '09 astral dynamics', artist: 'oneish', file: 'assets/audio/09.mp3' },
-  { title: '10 the holographic universe', artist: 'oneish', file: 'assets/audio/10.mp3' },
-  { title: '11 a course in miracles', artist: 'oneish', file: 'assets/audio/11.mp3' }
+  { title: '01 i am a strange loop', artist: 'oneish', file: 'assets/audio/01-i am a strange loop.wav' },
+  { title: '02 incognito', artist: 'oneish', file: 'assets/audio/02-incognito.wav' },
+  { title: '03 lsd and the mind of the universe', artist: 'oneish', file: 'assets/audio/03-lsd and the mind of the universe.wav' },
+  { title: '04 storming heaven', artist: 'oneish', file: 'assets/audio/04-storming heaven.wav' },
+  { title: '05 the cosmic serpent', artist: 'oneish', file: 'assets/audio/05-the cosmic serpent.wav' },
+  { title: '06 real magic', artist: 'oneish', file: 'assets/audio/06-real magic.wav' },
+  { title: '07 prometheus rising', artist: 'oneish', file: 'assets/audio/07-prometheus rising.wav' },
+  { title: '08 monsters and magical sticks', artist: 'oneish', file: '08-monsters and magical sticks.wav' },
+  { title: '09 astral dynamics', artist: 'oneish', file: 'assets/audio/09-astral dynamics.wav' },
+  { title: '10 the holographic universe', artist: 'oneish', file: 'assets/audio/10-the holographic universe.wav' },
+  { title: '11 a course in miracles', artist: 'oneish', file: 'assets/audio/11-a course in miracles.wav' }
 ];
